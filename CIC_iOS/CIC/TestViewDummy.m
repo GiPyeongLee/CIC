de52547acc92fed13af62805240d0d872146042f
GiPyeong <lkp0907@gmail.com>
2014-11-26 16:09:27 +0900

    add Test Dummy

---
+++ CIC_iOS/CIC/TestViewDummy.m
@@ -0,0 +1,23 @@
+//
+//  TestViewDummy.m
+//  CIC
+//
+//  Created by GiPyeong Lee on 2014. 11. 26..
+//  Copyright (c) 2014년 com.devsfolder.cic. All rights reserved.
+//
+
+#import "TestViewDummy.h"
+
+
+@implementation TestViewDummy
+
+#pragma mark - Hello World
+- (id)init{
+    self = [super init];
+    if(self){
+        
+    }
+    return self;
+}
+
+@end
